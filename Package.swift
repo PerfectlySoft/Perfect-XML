a9343cd34b0feaa9fc21478136ca577c8b2eb73b
Kyle Jessup <badthing@gmail.com>
2016-09-06 15:52:49 -0400

    Pinned dep versions

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
 	name: "PerfectXML",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-libxml2.git",
-		         versions: Version(0,0,0)..<Version(10,0,0))
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-libxml2.git", majorVersion: 2, minor: 0)
 	],
 	exclude: []
 )
